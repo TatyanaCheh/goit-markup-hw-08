6a3edecb4174bb6db9e0b2c9b9859024ee30b30d
TatyanaCheh <tatyana.cheh1@gmail.com>
2022-11-27 09:59:46 +0200

    modal

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,5 +1,5 @@
 (() => {
-    const mobileMenu = document.querySelector('.js-menu-container');
+    const mobileMenu = document.querySelector('.drop-menu');
     const openMenuBtn = document.querySelector('.js-open-menu');
     const closeMenuBtn = document.querySelector('.js-close-menu');
   
@@ -9,20 +9,10 @@
       openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
       mobileMenu.classList.toggle('is-open');
   
-      const scrollLockMethod = !isMenuOpen
-        ? 'disableBodyScroll'
-        : 'enableBodyScroll';
-      bodyScrollLock[scrollLockMethod](document.body);
+  
     };
   
     openMenuBtn.addEventListener('click', toggleMenu);
     closeMenuBtn.addEventListener('click', toggleMenu);
   
-    // Close the mobile menu on wider screens if the device orientation changes
-    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-      if (!e.matches) return;
-      mobileMenu.classList.remove('is-open');
-      openMenuBtn.setAttribute('aria-expanded', false);
-      bodyScrollLock.enableBodyScroll(document.body);
-    });
   })();
